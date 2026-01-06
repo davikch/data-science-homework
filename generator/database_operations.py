71e8b2374b221fcfbb0fe206bf8b9fed1ac1ad3c
Davikch <davidogluzd@gmail.com>
2026-01-06 21:25:28 +1000

    Reorganize db operations code
    Basically move args handling outside of sqlalchemy session

--- generator/database_operations.py
+++ generator/database_operations.py
@@ -4,15 +4,16 @@ from time import sleep
 import sqlalchemy
 from generate_data import generate_entry
 
+
+if len(sys.argv) < 2:
+    BATCH_SIZE = 100
+else:
+    BATCH_SIZE = sys.argv[1]
+
 # https://docs.sqlalchemy.org/en/20/core/engines.html#postgresql
 engine = sqlalchemy.create_engine("postgresql://postgres@localhost/taxis", echo=True)
 
 with sqlalchemy.orm.Session(engine) as session:
-    if len(sys.argv) < 2:
-        BATCH_SIZE = 100
-    else:
-        BATCH_SIZE = sys.argv[1]
-
     while True:
         batch = [generate_entry() for _ in range(BATCH_SIZE)]
         session.add_all(batch)
