37c1e36c3b74b76f195d22d6b6c592d6b9eeb32a
Davikch <davidogluzd@gmail.com>
2026-01-08 13:31:45 +1000

    Fix price random generation

--- generator/generate_data.py
+++ generator/generate_data.py
@@ -1,10 +1,15 @@
 import random
-import numpy as np
-
+import decimal
 from decimal import Decimal
+
+import numpy as np
 
 import models
 import names
+
+decimal.setcontext(
+    decimal.Context(prec=2),
+)
 
 
 def generate_entry() -> models.Ride:
@@ -13,16 +18,22 @@ def generate_entry() -> models.Ride:
         driver=random.choice(names.GIVEN_NAMES),
         price=_calculate_price(
             coefficient=_calculate_coefficient(random.randint(1, 10)),
-            urban_distance=min(np.random.normal(5, 2), 0.5),
-            nonurban_distance=np.random.normal(5, 3) * 1
-            if (random.randint(1, 10) >= 8)
-            else 0,
+            urban_distance=max(
+                np.random.normal(5, 2),
+                0.5,
+            ),
+            nonurban_distance=max(
+                np.random.normal(7, 3) * 1 if (random.randint(1, 10) >= 8) else 0,
+                0,
+            ),
         ),
-        destination=f"{random.choice(names.STREETS)}, {random.randint(1,150)}",
+        destination=f"{random.choice(names.STREETS)}, {random.randint(1, 150)}",
     )
 
 
-def _calculate_price(coefficient: int, urban_distance: float, nonurban_distance: float):
+def _calculate_price(
+    coefficient: int, urban_distance: float, nonurban_distance: float
+) -> Decimal:
     PRICE_PER_KM = 100
     return Decimal(
         coefficient * PRICE_PER_KM * (urban_distance + 1.5 * nonurban_distance)
@@ -33,4 +44,4 @@ def _calculate_coefficient(seed: int) -> Decimal:
     if seed < 7:
         return 1
     else:
-        return 3
+        return 1.75
