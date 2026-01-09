a44c1dea3e934e055dc5ba153c7de4eeb3163ca6
Davikch <davidogluzd@gmail.com>
2026-01-09 17:42:59 +1000

    Add .env handling to generator; update requirements.txt

--- generator/database_operations.py
+++ generator/database_operations.py
@@ -3,6 +3,7 @@ from time import sleep
 
 import sqlalchemy
 from generate_data import generate_entry
+from get_env import PASSWORD, DB, USER
 import models
 
 
@@ -12,7 +13,9 @@ else:
     BATCH_SIZE = int(sys.argv[1])
 
 # https://docs.sqlalchemy.org/en/20/core/engines.html#postgresql
-engine = sqlalchemy.create_engine("postgresql://postgres@localhost/taxis")
+engine = sqlalchemy.create_engine(
+    f"postgresql://{USER}:{PASSWORD}@localhost/{DB}"
+)
 models.Base.metadata.create_all(engine)
 
 with sqlalchemy.orm.Session(engine) as session:
---
+++ generator/get_env.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv("../database/.env")
+
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB = os.getenv("POSTGRES_DB")
+USER = os.getenv("POSTGRES_USER", "postgres")
